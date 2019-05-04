- fixed influence computation with Hessians; it works now entirely when a special function of the SPFlow library is modified in two certain lines - removed code used for debugging (such as print())

src/influence/genericNeuralNet.py
@@ -104,19 +104,16 @@ class GenericNeuralNet(object):
             os.makedirs(self.train_dir)
 
         # Initialize session
-        print("Initialize session")
         config = tf.ConfigProto()
         self.sess = tf.Session(config=config)
         K.set_session(self.sess)
 
         # Setup input
-        print("Setup input")
         self.input_placeholder, self.labels_placeholder = self.placeholder_inputs()
         self.num_train_examples = self.data_sets.train.labels.shape[0]
         self.num_test_examples = self.data_sets.test.labels.shape[0]
 
         # Setup inference and training
-        print("Setup inference and training")
         if self.keep_probs is not None:
             self.keep_probs_placeholder = tf.placeholder(tf.float32, shape=(2))
             self.logits = self.inference(self.input_placeholder, self.keep_probs_placeholder)
@@ -125,7 +122,6 @@ class GenericNeuralNet(object):
         else:
             self.logits = self.inference(self.input_placeholder)
 
-        print("Setup loss")
         self.total_loss, self.loss_no_reg, self.indiv_loss_no_reg = self.loss(
             self.logits,
             self.labels_placeholder)
@@ -134,8 +130,6 @@ class GenericNeuralNet(object):
         # self.learning_rate = tf.Variable(self.initial_learning_rate, name='learning_rate', trainable=False)
         # self.learning_rate_placeholder = tf.placeholder(tf.float32)
         # self.update_learning_rate_op = tf.assign(self.learning_rate, self.learning_rate_placeholder)
-
-        print("Trainable variables:", tf.trainable_variables())
 
         # self.train_op = self.get_train_op(self.total_loss, self.global_step, self.learning_rate)
         # self.train_sgd_op = self.get_train_sgd_op(self.total_loss, self.global_step, self.learning_rate)
@@ -148,41 +142,20 @@ class GenericNeuralNet(object):
         # self.saver = tf.train.Saver()
 
         # Setup gradients and Hessians
-        print("Setup gradients and Hessians")
         self.params = self.get_all_params()
-        # print("self.params:", self.params)
-        print("grad_total_loss_op...")
         self.grad_total_loss_op = tf.gradients(self.total_loss, self.params)
-        # print("self.grad_total_loss_op:", self.grad_total_loss_op)
-        print("grad_loss_no_reg_op...")
         self.grad_loss_no_reg_op = tf.gradients(self.loss_no_reg, self.params)
         self.v_placeholder = [tf.placeholder(tf.float32, shape=a.get_shape()) for a in self.params]
         self.u_placeholder = [tf.placeholder(tf.float32, shape=a.get_shape()) for a in self.params]
-        # print("self.v_placeholder:", self.v_placeholder)
-        # print("self.u_placeholder:", self.u_placeholder)
-
-        '''
-        with tf.Session() as sess:
-            tf.global_variables_initializer().run()
-            writer = tf.summary.FileWriter("../output", sess.graph)
-            print(sess.run(self.total_loss,
-                           feed_dict={self.input_placeholder: [self.data_sets.train.x[0]], self.labels_placeholder: [1]}))
-            writer.close()'''
-
-        # TODO: Resolve LookupError
         self.hessian_vector = hessian_vector_product(self.total_loss, self.params, self.v_placeholder)
-
-        print("grad_loss_wrt_input_op...")
         self.grad_loss_wrt_input_op = tf.gradients(self.total_loss, self.input_placeholder)
 
         # Because tf.gradients auto accumulates, we probably don't need the add_n (or even reduce_sum)
-        print("influence_op...")
         self.influence_op = tf.add_n(
             [tf.reduce_sum(tf.multiply(a, array_ops.stop_gradient(b))) for a, b in
              zip(self.grad_total_loss_op, self.v_placeholder)])
 
-        print("grad_influence_wrt_input_op...")
-        # TODO: Runs out of memory
+        # TODO?: Runs out of memory
         self.grad_influence_wrt_input_op = tf.gradients(self.influence_op, self.input_placeholder)
 
         # self.checkpoint_file = os.path.join(self.train_dir, "%s-checkpoint" % self.model_name)
@@ -190,7 +163,6 @@ class GenericNeuralNet(object):
         self.all_train_feed_dict = self.fill_feed_dict_with_all_ex(self.data_sets.train)
         self.all_test_feed_dict = self.fill_feed_dict_with_all_ex(self.data_sets.test)
 
-        print("Initialize global variables")
         init = tf.global_variables_initializer()
         self.sess.run(init)
 

src/scripts/setup_multi_leaf.py
@@ -3,10 +3,9 @@ if __name__ == '__main__':  # needed to circumvent multiprocessing RuntimeError 
     from src.influence.dataset import DataSet  # for train and test set creation
     from tensorflow.contrib.learn.python.learn.datasets import base
     from spn.structure.leaves.parametric.Parametric import Gaussian, Categorical  # for SPN structure
-    import numpy as np
     from src.help_functions import *
 
-    # Generate dummy data sets (at least 5 instances needed for HVP, why ever)
+    # Generate dummy data sets (at least 5 instances needed for HVP, why soever...)
     train_set = DataSet(np.array([[1.0], [1.5], [0.9], [0.77], [0.66], [1.15]], dtype=np.float32),
                         np.array([[1], [1], [0], [0], [0], [1]], dtype=np.float32))
     test_samples = np.array([[1.9], [0.4]], dtype=np.float32)
@@ -21,33 +20,24 @@ if __name__ == '__main__':  # needed to circumvent multiprocessing RuntimeError 
     batch_size = 1
 
     # Construct multi-leaf SPN
-    # Throws LookupError because Categorical is not twice differentiable
     multi_leaf_spn = 0.1 * (Gaussian(mean=[0.67], stdev=[0.22], scope=0) *
                             Categorical(p=[0.2, 0.8], scope=1)) \
                      + 0.9 * (Gaussian(mean=[0.39], stdev=[0.99], scope=0) *
                               Categorical(p=[0.2, 0.8], scope=1))
-    '''
-    multi_leaf_spn = 0.1 * (Gaussian(mean=[0.67], stdev=[0.22], scope=0) *
-                            Gaussian(mean=[0.57], stdev=[0.11], scope=1)) \
-                     + 0.9 * (Gaussian(mean=[0.39], stdev=[0.99], scope=0) *
-                              Gaussian(mean=[0.07], stdev=[0.18], scope=1))'''
 
     # Convert this SPN into a tf.Tensor (test_samples needed for shape)
-    single_leaf_spn_tensor, data_placeholder, _ = convert_spn_to_tf_graph(multi_leaf_spn,
-                                                                          np.concatenate((test_samples, test_labels),
-                                                                                         axis=1),
-                                                                          batch_size,
-                                                                          dtype=np.float32)
+    single_leaf_spn_tensor, _, _ = convert_spn_to_tf_graph(multi_leaf_spn,
+                                                           np.concatenate((test_samples, test_labels), axis=1),
+                                                           batch_size,
+                                                           dtype=np.float32)
 
     root = tf.identity(single_leaf_spn_tensor, name="Root")
 
-    '''
-    sample_placeholder = data_placeholder[:, 0]
-    label_placeholder = data_placeholder[:, 1]'''
-
     # Export the model
-    export_dir = "output/converted_single_leaf_spn"
-    export_dir = export_model(export_dir=export_dir)
+    # root_dir = "/tmp/Projects/Interpreting-SPNs"
+    root_dir = "C:/Users/markr/Google Drive/[00] UNI/[00] Informatik/BA/Interpreting SPNs"
+    export_dir = "/output/spns/converted_multi_leaf_spn"
+    import_dir = export_model(root_dir=root_dir, export_dir=export_dir)
 
     tf.reset_default_graph()
 
@@ -58,18 +48,17 @@ if __name__ == '__main__':  # needed to circumvent multiprocessing RuntimeError 
     label_placeholder = tf.placeholder(dtype=np.float32,
                                        shape=(batch_size, test_labels.shape[1]),
                                        name="Label_Placeholder")
-    # no_gradient = tf.stop_gradient(label_placeholder)
     input_placeholder = tf.concat([sample_placeholder, label_placeholder], 1)
     input_map = {"Placeholder:0": input_placeholder}
-    restored_spn_graph = import_model(export_dir, input_map)
-    root = restored_spn_graph.get_tensor_by_name("Root:0")
+    restored_spn_graph = import_model(import_dir, input_map)
+    new_root = restored_spn_graph.get_tensor_by_name("Root:0")
 
-    # Create a graph log to visualize graph with TensorBoard
-    plot_tf_graph(root, {sample_placeholder: [test_samples[1]],
-                         label_placeholder: [test_labels[1]]})
+    # Create a graph log to visualize the TF graph with TensorBoard
+    plot_tf_graph(new_root, {sample_placeholder: [test_samples[1]],
+                             label_placeholder: [test_labels[1]]})
 
     # Initialize single-leaf SPN
-    interpretable_spn = InterpretableSpn(root_node=root,
+    interpretable_spn = InterpretableSpn(root_node=new_root,
                                          input_placeholder=sample_placeholder,
                                          label_placeholder=label_placeholder,
                                          data_sets=data_sets,
@@ -79,9 +68,10 @@ if __name__ == '__main__':  # needed to circumvent multiprocessing RuntimeError 
                                          batch_size=batch_size,
                                          num_epochs=15,
                                          model_name=model_name,
-                                         train_dir='output',
+                                         train_dir=root_dir+'/output/training',
                                          mini_batch=False)
 
+    # Experimental influence computations
     influence = interpretable_spn.get_influence_on_test_loss(test_indices=[1],
                                                              train_idx=[0],
                                                              ignore_hessian=False)